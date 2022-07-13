523d84dc0e7127bdbb132c2bcf8cb0159d19a455
Reiner Hüchting <dev@reinerhuechting.de>
2022-07-14 01:15:14 +0200

    Test korrigiert.

--- element/rotations_test.go
+++ element/rotations_test.go
@@ -71,7 +71,7 @@ func ExampleRotateRightLeft() {
 	t1.Add(25)
 
 	fmt.Println(t1.AllMermaidEdges(""))
-	t1 = RotateLeftRight(t1)
+	t1 = RotateRightLeft(t1)
 	fmt.Println(t1.AllMermaidEdges(""))
 
 	// Output:
